879947d4b13bfa559d83a603477462ef1d69e6e6
Kai Pimenta <kai.pimenta1@gmail.com>
2022-05-27 11:24:59 -0300

    feature: requesting and mapping API data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const API_KEY = "cd3fbaf6482e418e83ff450578540d57"; 
+  let today = new Date();
+  let date = `${today.getFullYear()}-${(today.getMonth()+1)}-${today.getDate()}`;
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://newsapi.org/v2/everything?q=Apple&from=${date}&sortBy=popularity&apiKey=${API_KEY}`)
+     .then((response) => response.json())
+     .then((actualData) => {
+       console.log('actual', actualData)
+      setData(actualData.articles);
+    })
+   }, []);
+
+  console.log(data)
+
   return (
     <div className="App">
-      alo brasil      
+      <ul>
+      {data &&
+          data.map(({ id, title, urlToImage }) => (
+            <li key={id}>
+              <h3>{title}</h3>
+              <img src={urlToImage}/>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
