ddc7d91493a1f67f2e1972c288c9d198b59f18e0
Kai Pimenta <kai.pimenta1@gmail.com>
2022-05-30 14:53:39 -0300

    fix: sorting more than one entity

--- src/components/OrderBy/index.js
+++ src/components/OrderBy/index.js
@@ -3,15 +3,20 @@ import './styles.css'
 
 const OrderBy = ({data, setData}) => {
 
-    const handleSort = () => {
+    const handleSort = (e) => {
         const sortedData = [...data].sort((a, b) => {
-            return a.author > b.author ? 1 : -1
+            return a[e.target.value] > b[e.target.value] ? 1 : -1
         })
         setData(sortedData)
     }
     return(
         <>
-            <button className="button__orderby" onClick={handleSort}>Order by Author</button>
+            <select className="button__orderby" onChange={handleSort}>
+                <option value="" disabled selected>Select your option</option>
+                <option value="author">Author</option>
+                <option value="publishedAt">Date</option>
+                <option value="title">Title</option>
+            </select>
         </>
     );
 }
