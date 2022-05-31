3f96fd19b2d5ee1912c5b6e69d4f2fc128fb9a98
Kai Pimenta <kai.pimenta1@gmail.com>
2022-05-31 13:47:50 -0300

    fix: adjusting route for pages

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path="/" element={
             <Home 
